6c2c72a5687a5e3d28a6e336997fa548787f41cf
nktshjhb025 <nktshjhb025@student.wethinkcode.co.za>
2026-02-09 09:20:57 +0200

    Added codewars

---
+++ codewars/pete.py
@@ -0,0 +1,26 @@
+
+def main():
+    available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+    recipe =  {"flour": 500, "sugar": 200, "eggs": 1}
+    print(cakes(recipe, available))
+
+def cakes(recipe, available):
+    rec_ = set(recipe)
+    ava_ = set(available)
+    cost_of_one = recipe.values()
+
+
+    no_of_cakes = 0
+
+    if len(rec_.difference(ava_)) == 0:
+        for a in recipe.keys():
+            if available[a] >= recipe[a]:
+                print(available[a], recipe[a])
+            else:
+                return no_of_cakes
+ 
+    else:
+        return no_of_cakes
+
+
+main()
