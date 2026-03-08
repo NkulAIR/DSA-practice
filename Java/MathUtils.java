31af3e2bc5e681efb0eaddebc225c583445dd306
nktshjhb025 <nktshjhb025@student.wethinkcode.co.za>
2026-03-08 17:16:58 +0200

    Added basicfor loop

---
+++ Java/MathUtils.java
@@ -0,0 +1,11 @@
+public class MathUtils {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        String name = "Nkuli";
+
+        for (int i = 0; i < name.length(); i ++) {
+            System.out.println(name.charAt(i));
+        }
+        
+    }
+}
---
+++ Java/Nkuli.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nkuli {
+    public static void main(String[] args) {
+        String greeting = "Hello Nkululeko - Welcome to Java\n";
+        System.out.println(greeting);
+
+        // Making an array - fixed size iterable / data structure
+        int[] number = {1,2,3,4,5};
+        System.out.println(even(number));
+
+    }
+    public static int even(int[] numbers) {
+        return numbers.length;
+
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
